4d64515a4686242fcb3a25ce4b2fa4c831841ffa
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-03-29 14:43:48 -0700

    adds submit event listener,for username

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
 
+//show input error message
+function showError(input,message) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control-error' ;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
 
+//Event Listeners
 form.addEventListener('submit',function(e) {
-    console.log('submit');
-} )
+    e.preventDefault();
+
+
+    if(username.value === '') {
+        showError(username,'Username is required');
+
+    } else {
+        showSuccess(username);
+    }
+} );
