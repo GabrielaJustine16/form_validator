9cc2168623a18e1e9246b2bdddbcd514fb0abf70
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-03-30 09:54:51 -0700

    fixes error so proper error message appears in form control

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
 
 //show input error message
-function showError(input,message) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control-error' ;
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error' ;
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
 //Event Listeners
-form.addEventListener('submit',function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
 
 
     if(username.value === '') {
         showError(username,'Username is required');
-
     } else {
         showSuccess(username);
     }
-} );
+});
